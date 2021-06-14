be804209a0f6197cea6612241446d419f04900bf
Kalvin <kalvinhart@hotmail.co.uk>
2021-06-14 16:15:01 +0100

    Updated GlobalStyles.

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -15,7 +15,11 @@ const GlobalStyle = createGlobalStyle`
 
     body {
         min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         font-family: Open Sans, sans-serif;
+        font-size: 16px;
         background: #fd746c;  /* fallback for old browsers */
         background: -webkit-linear-gradient(to right, #ff9068, #fd746c);  /* Chrome 10-25, Safari 5.1-6 */
         background: linear-gradient(to right, #ff9068, #fd746c); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
